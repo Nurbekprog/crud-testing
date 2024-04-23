83035801b635cf100028f60a924fc58f5fe54b89
nurbekdevs <nurbekdeveloper@gmail.com>
2024-04-23 07:55:55 +0500

    decoration

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import TodoList from './components/TodoList'
 
 export default function App() {
     return ( 
-      <div className='flex justify-center items-center my-20'>
+      <div>
+      <h1 className='text-center text-zinc-900 font-bold text-5xl pt-5'>Todo List</h1>
+      <div className='flex justify-center items-center my-20'>  
         <div className='flex flex-col gap-6'>
           <TodoForm/>
           <TodoList/>
-        </div>
-        
+        </div> 
+      </div>
       </div>
     )
 
