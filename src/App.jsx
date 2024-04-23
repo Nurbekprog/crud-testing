413870d1d4d9364cb1e71864164ee7ab5ba091b2
Nurbekprog <nurprogdata@gmail.com>
2024-04-23 08:01:57 +0500

    decoration

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+
+
+
+export default function App() {
+    return ( 
+      <div>
+      <h1 className='text-center text-zinc-900 font-bold text-4xl'>Todo List</h1>
+      <div className='flex justify-center items-center my-20'>  
+        <div className='flex flex-col gap-6'>
+          <TodoForm/>
+          <TodoList/>
+        </div> 
+      </div>
+      </div>
+    )
+
+}
